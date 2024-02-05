79630c7a7dd857554eccc6f61786178a628371c6
Krzysztof Andrelczyk <cristof@curiana.net>
2024-02-05 23:46:13 +0100

    capabilities fix and console panic hook

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use components::App;
 use leptos::*;
 
 fn main() {
+    console_error_panic_hook::set_once();
 
     _ = console_log::init_with_level(log::Level::Debug);
     mount_to_body(move || {
